e086deb394b514130bcc571f862db427904d787e
Daniel Páez <danielpaezsw66@gmail.com>
2019-01-27 19:31:16 -0300

    Mejorando la lectura del código de promesas

--- src/js/home.js
+++ src/js/home.js
@@ -1,66 +1,118 @@
-console.log('hola mundo!');
-const nombreUsuario = "Daniel";
+/*
+Autor: Daniel Páez
+Titulo: De jquery a javascript
+Descripcion: pasando De jquery a javascript
+*/
 
+/*INICIALIZACION DE ARCHIVO
+======================================================*/
+
+console.log('Inicializando archivo');
+
+/*DECLARACION DE VARIABLES
+=========================================================*/
+
+const nombreUsuario = "Daniel";
 let usuario = "@heysoypaez"
+
+
+
+/* DECLARACION DE FUNCIONES
+=========================================================*/
 
 function cambiarNombre(nuevoNombre) {
   cambia = nuevoNombre
 }
 
 
+/* DECLARACION DE PROMESAS
+=========================================================*/
+
+
 //Creando una promesa
-const getUser = new Promise(function(todoChevere,seJodio) {
+const getUser = new Promise(function(todoChevere,error) {
 
 	// llamar un api
 	setTimeout(function(){
 		//Luego de 3 segundos se ejecuta esto
-		todoChevere("GANE? No, llegue en 5 segundos");
-	}, 5000)
+		todoChevere("GANE? No, llegue en 3 segundos");
+	}, 3000)
+
+	if(!todoChevere) {
+
+	error("Damas y caballeros... hubo un error")
+	
+	}
 
 })
 
 //Creando una promesa
-const getUserAll = new Promise(function(todoChevere,seJodio) {
+const getUserAll = new Promise(function(todoChevere,error) {
 
 	// llamar un api
-	setTimeout(function(){
+	setTimeout( function() {
 		//Luego de 3 segundos se ejecuta esto
-		todoChevere("GANE A LOS 3 SEGUNDOS");
-	}, 3000)
+		todoChevere("GANE A LOS 2 SEGUNDOS");
+	}, 2000)
 
+	if(!todoChevere) {
+
+	error("Damas y caballeros... hubo un error")
+
+	}
 
 	
 })
+
+
+/*EJECUCION (CONSUMO) DE PROMESAS
+=========================================================*/
+
+
+	/*PROMISE.RACE()
+	Este método envia a la promesa que termine primero */
+
+	Promise.race([
+	getUser,
+	getUserAll
+	])
+
+	.then(function(mensaje) {
+	console.log(mensaje)
+	})
+
+
 
 
 //Metodos si todo va bien
 getUser
-	.then( function () {
-		// body...
-		console.log("Todo esta bien en la vida")
-	})
-	.catch( function(mensaje) {
-		console.error(mensaje)
+	.then( function (mensaje) {
+
+	console.log(`HEY ${mensaje}`)
+
 	})
 
-	//Enviando varias promesas al mismo tiempo
-		Promise.race([
-			getUser,
-			getUserAll,
-			getUserAll,
-			
-			])
+	
+	/* PROMISE.ALL() 
+	Espera a que todas las promesas esten listas 
+	y las ejecuta todas en el orden declarado */
 
-		//solo entramos al then de la promesa que se resuelva primero
-		.then(function(mensaje) {
-			console.log(mensaje)
-		})
+	Promise.all([
+	getUser,
+	getUserAll,
+	])
 
-		.catch(function (mensaje) {
-			console.log(mensaje)
-		})
+	.then(function(mensaje) {
+
+	console.log(`Ya en serio ${mensaje}`)
+
+	})
 	
 
-//promise. all espera a que todo este y te lo manda en el orden que dijiste
+	
+	//Metodo de Promise cuando las cosas van mal
+	.catch( function(mensaje) {
 
+	console.error(mensaje)
 
+	})
