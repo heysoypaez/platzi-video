bbbf8e54d3885a6baeb4937a8976d37f92745e52
Daniel Páez <danielpaezsw66@gmail.com>
2019-01-27 11:24:47 -0300

    Clase 2, Promesas realizada

--- src/js/home.js
+++ src/js/home.js
@@ -14,7 +14,7 @@ const getUser = new Promise(function(todoChevere,seJodio) {
 	// llamar un api
 	setTimeout(function(){
 		//Luego de 3 segundos se ejecuta esto
-		todoChevere("Se acabó el tiempo");
+		todoChevere("GANE? No, llegue en 5 segundos");
 	}, 5000)
 
 })
@@ -25,7 +25,7 @@ const getUserAll = new Promise(function(todoChevere,seJodio) {
 	// llamar un api
 	setTimeout(function(){
 		//Luego de 3 segundos se ejecuta esto
-		todoChevere("Se acabó el tiempo");
+		todoChevere("GANE A LOS 3 SEGUNDOS");
 	}, 3000)
 
 
@@ -44,12 +44,14 @@ getUser
 	})
 
 	//Enviando varias promesas al mismo tiempo
-		Promise.all([
+		Promise.race([
 			getUser,
 			getUserAll,
 			getUserAll,
 			
 			])
+
+		//solo entramos al then de la promesa que se resuelva primero
 		.then(function(mensaje) {
 			console.log(mensaje)
 		})
