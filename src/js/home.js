7c31789c994dd3f1059b3dbded1d44f2cf024639
Daniel Páez <danielpaezsw66@gmail.com>
2019-02-04 22:36:02 -0300

    Clase 6, Creacion de templates

--- src/js/home.js
+++ src/js/home.js
@@ -192,6 +192,27 @@ fetch(requestIronMan)
 
 
 
+
+function videoItemTemplate(src , title) {
+	return(
+		 `<div class="primaryPlaylist">
+            <h3 class="primaryPlaylist-topic">Para llorar</h3>
+            <h2 class="primaryPlaylist-title">Drama</h2>
+            <div class="primaryPlaylist-list" id="drama">
+              <div class="primaryPlaylistItem">
+                <div class="primaryPlaylistItem-image">
+                  <img src=${src}>
+                </div>
+                <h4 class="primaryPlaylistItem-title">
+                  ${title}
+                </h4>
+              </div>
+            </div>
+          </div>`
+		)
+}
+
+
 /*
 $.ajax({
   url: 'https://randomuser.me/api/',
@@ -234,6 +255,51 @@ $.ajax({
 	console.log("actionList", actionList);
 	console.log("terrorList", terrorList);
 	console.log("animationList", animationList);
+
+	/*SELECTORES
+	k=============*/
+
+	//llamando a los elementos home con jquery
+	//$("home")
+
+	const $featuringContainer = document.querySelector("#featuring") 
+	const $form = document.querySelector("#form") 
+	const $home = document.querySelector("#home") 
+
+//El peso es una convención de que es un elemento del DOM
+	const $modal = document.getElementById("modal")
+
+
+	const $mContainer = document.querySelector("#action")
+	const $animationContainer = document.getElementById("animation")
+	const $terrorContainer = document.getElementById("terror")	
+
+	
+const $overlay = document.getElementById("overlay")
+const $hideModal = document.getElementById("hide-modal")
+
+//para optimizar el codigo buscamos en modal
+const $modalImage =  $modal.querySelector("img")
+const $modalTitle =  $modal.querySelector("h1")
+const $modalDescription =  $modal.querySelector("p")
+
+
+
+/*
+queryselectorall	
+queryselector
+queryselectorall
+*/
+//nos permite seleccionar el primer elemento que coincida .algo ·algo algo
+//trae solo el primer elemento
+
+
+	actionList.data.movies.forEach( (movie) => {
+	const HTMLString = videoItemTemplate(movie);
+	console.log(HTMLString)
+})
+
+
 })() //esta funcion que se autoejecuta gracias a los ultimos parentesis
 
 
@@ -242,3 +308,11 @@ $.ajax({
 /*URLs APIs
 https://yts.am/api
 */
+
+
+
+//console.log(videoItemTemplate("src/images/covers/bitcoin.jpg","bitcoin") )
+        
+          //la comilla inversa acepta multilinea
+
+
